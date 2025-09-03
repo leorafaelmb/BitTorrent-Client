5d48f7242fee9da92d9af253c5b019ea815190d9
leorafaelmb <leorafaelmb@gmail.com>
2025-09-03 13:05:43 -0400

    Implement decodeList, decoding bencoded lists

--- app/main.go
+++ app/main.go
@@ -15,51 +15,84 @@ var _ = json.Marshal
 // Example:
 // - 5:hello -> hello
 // - 10:hello12345 -> hello12345
-func decode(bencodedString string) (interface{}, error) {
-	identifier := rune(bencodedString[0])
+func decode(bencodedString string, index int) (interface{}, int, error) {
+	identifier := rune(bencodedString[index])
 	if unicode.IsDigit(identifier) {
-		return decodeString(bencodedString)
+		return decodeString(bencodedString, index)
 
 	} else if identifier == 'i' {
-		return decodeInt(bencodedString)
+		return decodeInt(bencodedString, index)
+
+	} else if identifier == 'l' {
+		return decodeList(bencodedString, index)
 
 	} else {
-		return "", fmt.Errorf("Only strings are supported at the moment")
+		return "", -1, fmt.Errorf("only strings, integers, and lists are supported at the moment")
 	}
 }
 
-func decodeString(bencodedString string) (string, error) {
+func decodeString(bencodedString string, index int) (string, int, error) {
 	var firstColonIndex int
 
-	for i := 0; i < len(bencodedString); i++ {
+	for i := index; i < len(bencodedString); i++ {
 		if bencodedString[i] == ':' {
 			firstColonIndex = i
 			break
 		}
 	}
-
-	lengthStr := bencodedString[:firstColonIndex]
+	//5:hello
+	lengthStr := bencodedString[index:firstColonIndex]
 
 	length, err := strconv.Atoi(lengthStr)
 	if err != nil {
-		return "", err
+		return "", -1, err
 	}
+	endIndex := firstColonIndex + 1 + length
 
-	return bencodedString[firstColonIndex+1 : firstColonIndex+1+length], nil
+	decodedString := bencodedString[firstColonIndex+1 : endIndex]
+
+	return decodedString, endIndex, nil
 }
 
-func decodeInt(bencodedString string) (int, error) {
-	i := 1
+func decodeInt(bencodedString string, index int) (int, int, error) {
+	i := index
 	for ; bencodedString[i] != 'e'; i++ {
-
 	}
 
-	decodedInt, err := strconv.Atoi(bencodedString[1:i])
+	decodedInt, err := strconv.Atoi(bencodedString[index+1 : i])
 	if err != nil {
-		return 0, err
+		return 0, -1, err
 	}
 
-	return decodedInt, nil
+	i++
+
+	return decodedInt, i, nil
+}
+
+func decodeList(bencodedString string, index int) ([]interface{}, int, error) {
+	decodedList := make([]interface{}, 0)
+	i := index
+	i++
+	for {
+		var val interface{}
+		var err error
+		fmt.Println(string(bencodedString[i]))
+
+		if bencodedString[i] == 'e' {
+			i++
+			break
+		}
+
+		val, i, err = decode(bencodedString, i)
+		if err != nil {
+			return nil, -1, err
+		}
+		decodedList = append(decodedList, val)
+
+	}
+
+	return decodedList, i, nil
+
 }
 
 func main() {
@@ -71,7 +104,7 @@ func main() {
 	if command == "decode" {
 		bencodedValue := os.Args[2]
 
-		decoded, err := decode(bencodedValue)
+		decoded, _, err := decode(bencodedValue, 0)
 		if err != nil {
 			fmt.Println(err)
 			return
