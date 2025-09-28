14316f75165864c1450fbf364856d98476b4e031
leorafaelmb <leorafaelmb@gmail.com>
2025-09-28 14:35:03 -0400

    Refactor tracker struct, methods and functions to tracker.go

---
+++ app/tracker.go
@@ -0,0 +1,109 @@
+package main
+
+import (
+	"encoding/binary"
+	"fmt"
+	"io"
+	"net/http"
+	"strings"
+)
+
+// TrackerRequest represents a request made to a tracker server
+type TrackerRequest struct {
+	TrackerURL string
+	InfoHash   string // urlencoded 20-byte long info hash
+	PeerId     string
+	Port       int
+	Uploaded   int
+	Downloaded int
+	Left       int
+	Compact    int
+}
+
+// newTrackerRequest serves as a constructor for the TrackerRequest struct.
+func newTrackerRequest(
+	trackerUrl string, infoHash string, peerId string, left int) *TrackerRequest {
+
+	return &TrackerRequest{
+		TrackerURL: trackerUrl,
+		InfoHash:   infoHash,
+		PeerId:     peerId,
+		Port:       6881,
+		Uploaded:   0,
+		Downloaded: 0,
+		Left:       left,
+		Compact:    1,
+	}
+}
+
+// getFullUrl returns the full url sent to a peer for a handshake
+func (treq TrackerRequest) getFullUrl() string {
+	return fmt.Sprintf(
+		"%s?info_hash=%s&peer_id=%s&port=%d&uploaded=%d&downloaded=%d&left=%d&compact=%d",
+		treq.TrackerURL, treq.InfoHash, treq.PeerId, treq.Port, treq.Uploaded, treq.Downloaded,
+		treq.Left, treq.Compact)
+}
+
+func (treq TrackerRequest) SendRequest() ([]byte, error) {
+	resp, err := http.Get(treq.getFullUrl())
+	if err != nil {
+		return nil, fmt.Errorf("error sending request to tracker server: %w", err)
+	}
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("error reading tracker response body: %w", err)
+	}
+	return body, nil
+}
+
+type TrackerResponse struct {
+	Interval int
+	Peers    []string
+}
+
+func newTrackerResponse(interval int, peers []string) *TrackerResponse {
+	return &TrackerResponse{
+		Interval: interval,
+		Peers:    peers,
+	}
+}
+
+func newTrackerResponseFromBytes(response []byte) (*TrackerResponse, error) {
+	decoded, _, err := decode(response, 0)
+	if err != nil {
+		fmt.Println("error decoding tracker response body: ", err)
+		return nil, err
+	}
+	d := decoded.(map[string]interface{})
+
+	var (
+		interval  = d["interval"].(int)
+		peerBytes = d["peers"].([]byte)
+		peers     []string
+	)
+
+	for i := 0; i < len(peerBytes); i += 6 {
+		port := binary.BigEndian.Uint16(peerBytes[i+4 : i+6])
+		address := fmt.Sprintf(
+			"%d.%d.%d.%d:%d", peerBytes[i], peerBytes[i+1], peerBytes[i+2], peerBytes[i+3],
+			port)
+		peers = append(peers, address)
+	}
+
+	return newTrackerResponse(interval, peers), nil
+}
+
+func (tres TrackerResponse) PeersString() string {
+	peers := tres.Peers
+	peersString := ""
+	for _, peer := range peers {
+		peersString += fmt.Sprintf("%s\n", peer)
+	}
+
+	return strings.TrimSpace(peersString)
+}
+
+func (tres TrackerResponse) getPeers() []string {
+	return tres.Peers
+}
